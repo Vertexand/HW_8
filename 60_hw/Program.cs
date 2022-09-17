c85a48d137e7f24092b51a30f0e9862f9e780861
Natalli <MobileVersion.Nataliayandx1212@yandex.by>
2022-09-17 09:42:21 +0300

    add 60

--- 60_hw/Program.cs
+++ 60_hw/Program.cs
@@ -4,7 +4,83 @@
 // 12(0,0,0) 22(0,0,1) 
 // 45(1,0,0) 53(1,0,1) 
 
-int[,,] GetMatrix(int rowsCount, int columnsCount, int YCount, int leftRange, int rightRange)
+Console.WriteLine($"\nВведите размер массива X x Y x Z:");
+int x = InputNumbers("Введите X: ");
+int y = InputNumbers("Введите Y: ");
+int z = InputNumbers("Введите Z: ");
+Console.WriteLine($"");
+if ((x * y * z) > 99)
+{
+    Console.WriteLine("введите двухзначное число");
+}
+else
+{
+    int[,,] array3D = new int[x, y, z];
+    CreateArray(array3D);
+    WriteArray(array3D);
+}
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void WriteArray(int[,,] array3D)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            Console.Write($"X({i}) Y({j}) ");
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Console.Write($"Z({k})={array3D[i, j, k]}; ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+void CreateArray(int[,,] array3D)
+{
+    int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+    int number;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < array3D.GetLength(0); x++)
+    {
+        for (int y = 0; y < array3D.GetLength(1); y++)
+        {
+            for (int z = 0; z < array3D.GetLength(2); z++)
+            {
+                array3D[x, y, z] = temp[count];
+                count++;
+            }
+        }
+    }
+}
+
+
+/* int[,,] GetMatrix(int rowsCount, int columnsCount, int YCount, int leftRange, int rightRange)//не работает пока, доделать
 {
     int[,,] matrix = new int[rowsCount, columnsCount, YCount];
 
@@ -31,23 +107,17 @@ void PrintMatrix(int[,,] matrix)
             for (int k = 0; k < matrix.GetLength(2); k++)
             {
                 // Console.Write(matrix[i, j, k] + " ");
-                Console.WriteLine($"{matrix[i,j,k]} {i} {j} {k} + "");
+                Console.WriteLine($"{matrix[i, j, k]} {i} {j} {k} + "");
             }
             Console.WriteLine();
         }
     }
 }
 int[,,] ourMatrix = GetMatrix(2, 2, 2, 10, 100);
-PrintMatrix(ourMatrix);
+PrintMatrix(ourMatrix); */
 
 
-
-
-
-
-
-
-/* Console.WriteLine($"\nВведите размер массива X x Y x Z:");
+/* Console.WriteLine($"\nВведите размер массива X x Y x Z:");//интернет
 int x = InputNumbers("Введите X: ");
 int y = InputNumbers("Введите Y: ");
 int z = InputNumbers("Введите Z: ");
@@ -115,13 +185,13 @@ void CreateArray(int[,,] array3D)
       }
     }
   }
-}
- */
+} */
 
 
 
 
-// Console.WriteLine("Введите число");//иван
+
+// Console.WriteLine("Введите число");//перевод в двух знач число
 // int N = Convert.ToInt32(Console.ReadLine());
 // GetBinaryView(N);//вызов метода важно 
 
@@ -130,3 +200,4 @@ void CreateArray(int[,,] array3D)
 //     if (number <= 0) return;//останов рекурсию выйдем из функции командой return
 //     GetBinaryView(number / 2);//вызов метода внутри метода это рекурсия
 //     Console.Write(number % 2);
+
